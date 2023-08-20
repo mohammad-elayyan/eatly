9c79fc7daef80044519326fb80472f22329381d2
mohammad-elayyan <mohmmadalean1@gmail.com>
2023-08-20 09:33:46 +0300

    upload files

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  $nav = $(".nav");
+  $toggle_collapse = $(".toggle-collapse .toggle-icons");
+
+  $toggle_collapse.click(function () {
+    $nav.toggleClass("collapse-nav");
+  });
+  splideSetup();
+
+  let options3 = {
+    perPage: 1,
+    rewind: false,
+  };
+  var splide3 = new Splide("#splide3", options3);
+  splide3.mount();
+});
+
+window.addEventListener("resize", splideSetup, false);
+window.addEventListener("blur", splideSetup, false);
+window.addEventListener("focus", splideSetup, false);
+
+function splideSetup() {
+  let perPage = 4;
+  let perPage2 = 2;
+  setTimeout(() => {
+    if (window.innerWidth <= 992 && window.innerWidth > 500) {
+      perPage = 2;
+    } else if (window.innerWidth <= 500) {
+      perPage = 1;
+      perPage2 = 1;
+    } else {
+      perPage = 4;
+      perPage2 = 2;
+    }
+
+    let options1 = {
+      perPage: perPage,
+      rewind: false,
+    };
+    let splide1 = new Splide("#splide1", options1);
+
+    splide1.mount();
+
+    let options2 = {
+      perPage: perPage2,
+      rewind: false,
+    };
+    let splide2 = new Splide("#splide2", options2);
+    splide2.mount();
+  }, 100);
+}
